4f4bf1b6961763cd059aca0ceae05578a2bac944
Daniel Mac <daniel.gm78@gmail.com>
2018-10-04 21:10:47 -0400

    Using AS to avoid conflicts with same names variables

--- exportdefault/main.js
+++ exportdefault/main.js
@@ -1,6 +1,11 @@
-import  rocketName, {COUNT_DOWN_DURATION, launch} from './rocket.js';
+import  rocketName, {COUNT_DOWN_DURATION, launch} from './saturn-v.js';
+import  falconName, {COUNT_DOWN_DURATION as falconCount, launch as falconLaunch} from './falcon-heavy.js';
+
 export function main() {
     console.log('This is a "%s" rocket', rocketName);
     console.log('It will launch in "%d" seconds', COUNT_DOWN_DURATION);
     launch();
+    console.log('This is a "%s" rocket', falconName);
+    console.log('It will launch in "%d" seconds', falconCount);
+    falconLaunch();
 }
